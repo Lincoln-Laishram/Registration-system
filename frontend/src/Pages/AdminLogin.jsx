cd064be36d3b380d358f6902fe34dc00eeac1d66
BimeshW <bimeshwangkheimayum@gmail.com>
2025-03-23 19:33:49 +0530

    add search with name and id in admin page

--- frontend/src/Pages/AdminLogin.jsx
+++ frontend/src/Pages/AdminLogin.jsx
@@ -2,55 +2,59 @@ import { useState, useEffect } from "react";
 import { PatientTable } from "./PatientTable";
 
 export const AdminLogin = () => {
-    const [admins, setAdmins] = useState([]);
-    const [state, setState] = useState(false);
-    const [inpt, setInpt] = useState({ admin_id: "", admin_password: "" });
-    const [loginMessage, setLoginMessage] = useState("");
+  const [admins, setAdmins] = useState([]);
+  const [state, setState] = useState(false);
+  const [inpt, setInpt] = useState({ admin_id: "", admin_password: "" });
+  const [loginMessage, setLoginMessage] = useState("");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`http://localhost:${import.meta.env.VITE_PORT}/admin`);
-                console.log(response);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                const data = await response.json();
-                setAdmins(data);
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        }
-        fetchData();
-    }, []);
-
-    const HandleChange = (e) => {
-        setInpt((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value,
-        }));
-    };
-
-    const HandleSubmit = async (e) => {
-        e.preventDefault();
-        const adminMatch = admins.find(admin =>
-            admin.admin_id === inpt.admin_id && admin.admin_password === inpt.admin_password
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:${import.meta.env.VITE_PORT}/admin`
         );
-
-        if (adminMatch) {
-            setLoginMessage("User admin welcome...");
-            setState(true);
-            console.log("User admin welcome...");
-        } else {
-            setLoginMessage("Please check your inputs...");
-            console.log("Please check your inputs...");
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
-    }
+        const data = await response.json();
+        setAdmins(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
-    return (
-        <>
-            {/* {!state ? */}
-                {/* <>
+  const HandleChange = (e) => {
+    setInpt((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+    const adminMatch = admins.find(
+      (admin) =>
+        admin.admin_id === inpt.admin_id &&
+        admin.admin_password === inpt.admin_password
+    );
+
+    if (adminMatch) {
+      setLoginMessage("User admin welcome...");
+      setState(true);
+      console.log("User admin welcome...");
+    } else {
+      setLoginMessage("Please check your inputs...");
+      console.log("Please check your inputs...");
+    }
+  };
+
+  return (
+    <>
+      {/* {!state ? */}
+      {/* <>
                     <div className="flex items-center justify-center h-screen w-screen">
                         <form onSubmit={HandleSubmit} className="flex flex-col bg-white p-6 rounded-lg shadow-gray-600 shadow-md max-w-lg w-full">
                             <input type="text" name="admin_id" placeholder="Enter ID" onChange={HandleChange} required className="input-field" />
@@ -61,9 +65,9 @@ export const AdminLogin = () => {
                     </div>
                 </> 
                 : */}
-                    <PatientTable />
-            
-            {/* } */}
-        </>
-    )
-}
+      <PatientTable />
+
+      {/* } */}
+    </>
+  );
+};
